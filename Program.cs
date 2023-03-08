83eab570df226eced071bc6384e986c03c64324f
AliCharper <alikolahdoozan@gmail.com>
2023-03-08 11:40:44 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//Parallel.For(0, 100, SampleMethod);
+
+//Parallel.For(0, 100, i=> 
+//{
+//    Console.WriteLine("Something...");
+//    Console.WriteLine(i.ToString());
+
+//});
+
+
+Parallel.For(0, 100, (i, state) =>
+{
+    Console.WriteLine($"Beginning iteration {i}");
+    Console.WriteLine(state.IsExceptional);
+});
+
+
+
+    //Parallel.For(0, 100, new ParallelOptions { MaxDegreeOfParallelism = 1 }, i =>
+    //{
+    //    Console.WriteLine(i.ToString());
+    //});
+
+
+
+
+    //Parallel.For(0, 100, delegate(int i)
+    //{
+    //    Console.WriteLine(i.ToString());
+    //});
+
+
+
+    //static void SampleMethod(int i)
+    //{
+    //    Console.WriteLine(i.ToString());
+    //}
+
+
+    Console.WriteLine("Finished!");
+
