214a6f13fa444e0c6667f820c097d9fb2825adbf
AliCharper <alikolahdoozan@gmail.com>
2023-03-09 11:58:38 +0800

    Parallel.Invoke

--- Program.cs
+++ Program.cs
@@ -44,20 +44,47 @@
 
 #region ParallelForEach
 
-var ArrayOfString = new[] {"1-Benz", "2-BMW", "3-Nisan", "4-Kia" };
+//var ArrayOfString = new[] {"1-Benz", "2-BMW", "3-Nisan", "4-Kia" };
 
-await Parallel.ForEachAsync(ArrayOfString, async (item,token) => 
-Console.WriteLine(await Add2023ToCarBrandAsync(item)));
+//await Parallel.ForEachAsync(ArrayOfString, async (item,token) => 
+//Console.WriteLine(await Add2023ToCarBrandAsync(item)));
 
 
-async Task<string> Add2023ToCarBrandAsync(string carBrand)
-{
-    return await Task.FromResult(carBrand+"-2023");
-}
+//async Task<string> Add2023ToCarBrandAsync(string carBrand)
+//{
+//    return await Task.FromResult(carBrand+"-2023");
+//}
 
 
 
 #endregion
+
+#region ParallelInvoke
+
+using System.Runtime.CompilerServices;
+
+//Parallel.Invoke(
+//    () => Console.WriteLine("1"),
+//    () => Console.WriteLine("2"),
+//    () => Console.WriteLine("3"),
+//    () => Console.WriteLine("4")
+//    );
+
+Parallel.Invoke(new ParallelOptions { MaxDegreeOfParallelism = 2 }, FirstJob, SecondJob);
+
+
+#endregion
+
+
+static void FirstJob()
+{
+    Console.WriteLine("First Job...");
+}
+
+static void SecondJob()
+{
+    Console.WriteLine("Second Job...");
+}
 
 
 Console.WriteLine("Finished!");
